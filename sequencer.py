dc276138615aac752f17382402c0a4ba611ccc8f
Daniel Morcuende <dmorcuen@ucm.es>
2020-03-04 09:58:51 +0000

    submit jobs with sbatch

--- sequencer.py
+++ sequencer.py
@@ -121,7 +121,7 @@ def single_process(telescope, process_mode):
 #    dot.writeworkflow(sequence_list)
     # Adds the scripts
     job.preparejobs(sequence_list, subrun_list)
-
+    
 #    queue_list = job.getqueuejoblist(sequence_list)
 #    veto_list = veto.getvetolist(sequence_list)
 #    closed_list = veto.getclosedlist(sequence_list)
@@ -129,7 +129,7 @@ def single_process(telescope, process_mode):
 #    updatesequencedb(sequence_list)
     # actually, submitjobs does not need the queue_list nor veto_list
 #    job_list = job.submitjobs(sequence_list, queue_list, veto_list)
-    job_list = job.submitjobs(sequence_list)
+#    job_list = job.submitjobs(sequence_list)
 #    combine_muon(job_list)
 #    # Report
 #    if is_report_needed:
