de69c720d864c11cf0defbd433bb3e86f045e124
Jose Enrique Ruiz <jer@iaa.es>
2020-04-15 14:00:03 +0200

    remove subruns in processing prov

--- provprocess.py
+++ provprocess.py
@@ -2,9 +2,11 @@
 Provenance post processing script for OSA pipeline
 """
 
-
+from osa.utils import cliopts, standardhandle
+from provenance.capture import get_file_hash, get_activity_id
 from provenance.io import *
-from pathlib import Path
+from pathlib import Path, PurePath
+import re
 
 
 def copy_used_file(src, out):
@@ -22,17 +24,49 @@ def copy_used_file(src, out):
     pass
 
 
-def parse_lines_dl1(prov_lines, out):
-    """Process r0 to dl1 provenance info to bundle session at run scope."""
-    for i in prov_lines:
-        if "used_file" in i:
-            copy_used_file("file", out)
-    return prov_lines
 
+def parse_lines_dl1(prov_lines, out, tag_handle):
+    """Process r0 to dl1 provenance info to bundle session at run scope."""
 
 def move_logfile(src, out):
     """Rename and move logfile"""
     pass
+    i = 0
+    size = 0
+    working_lines = []
+    id_activity_run = ""
+    for line in prov_lines:
+
+        # get info
+        i += 1
+        remove = False
+        endTime = line.get("endTime", "")
+        session_id = line.get("session_id", "")
+        activity_id = line.get("activity_id", "")
+        filepath = line.get("filepath", "")
+        used_role = line.get("generated_role", "")
+        generated_role = line.get("generated_role", "")
+        parameters = line.get("parameters", "")
+        name = line.get("name", "")
+
+        # remove subruns info
+        if "data/real/R0/" in filepath or used_role == "Observation subrun":
+            remove = True
+        if "data/real/DL1/" in filepath or generated_role == "DL1 subrun dataset":
+            remove = True
+        if parameters:
+            del line["parameters"]["ObservationSubRun"]
+        if name == "r0_to_dl1" and not session_id:
+            size += 1
+            if not id_activity_run:
+                id_activity_run = get_activity_id()
+        if size > 1:
+            remove = True
+
+        # new id
+        if activity_id:
+            line["activity_id"] = id_activity_run
+    return working_lines
 
 
 def make_json(filepath):
@@ -46,7 +80,7 @@ def make_graph(filepath):
 
 
 if __name__ == "__main__":
-    from osa.utils import cliopts, standardhandle
+
     options, tag = cliopts.provprocessparsing()
 
     # check options.src is a file
@@ -63,7 +97,7 @@ if __name__ == "__main__":
         options.out.mkdir()
 
     # process prov file
-    processed_lines = parse_lines_dl1(read_prov(logname=options.src), options.out)
+    processed_lines = parse_lines_dl1(read_prov(logname=options.src), options.out, tag)
 
     # build base_filename with options.run and options.out
     #
