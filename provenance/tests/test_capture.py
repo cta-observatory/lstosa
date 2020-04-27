664f6f2589e2e005ae517bf248c78f434b2ef638
Jose Enrique Ruiz <jer@iaa.es>
2020-04-27 20:29:07 +0200

    refactor args needed in r0_to_dl1 function

--- provenance/tests/test_capture.py
+++ provenance/tests/test_capture.py
@@ -39,23 +39,28 @@ def select_config(tmp_path):
     options.configfile = str(temp_config_path)
 
 
+def make_args_r0_to_dl1():
 
-    options.configfile = "cfg/sequencer.cfg"
     args = (
-    "/Users/jer/fefs/aswg/data/real/calibration/20200218/v00/calibration.Run2006.0000.hdf5",
-    "/Users/jer/fefs/aswg/data/real/calibration/20200218/v00/drs4_pedestal.Run2005.0000.fits",
-    "/Users/jer/fefs/aswg/data/real/calibration/20191124/v00/time_calibration.Run1625.0000.hdf5",
-    "/Users/jer/fefs/home/lapp/DrivePositioning/drive_log_20_02_18.txt",
-    "ucts_t0_dragon",
-    "dragon_counter0",
-    "ucts_t0_tib",
-    "tib_counter0",
-    "02006.0002",
-    "/Users/jer/fefs/aswg/data/real/DL1/20200218/v0.4.3_v00/sequence_LST1_02006.0001.history"
+        "calibration.Run2006.0000.hdf5",
+        "drs4_pedestal.Run2005.0000.fits",
+        "time_calibration",
+        "drivefile",
+        "ucts_t0_dragon",
+        "dragon_counter0",
+        "ucts_t0_tib",
+        "tib_counter0",
+        "02006.0002",
+        "DL1/20200218/v0.4.3_v00/historyfile/sequence_LST1_02006.0001.history"
     )
+    return args
+
+
 def test_trace_r0_to_dl1(tmp_path):
 
     # config
     select_config(tmp_path)
+    args = make_args_r0_to_dl1()
+
     # track prov
     r0_to_dl1(*args)
