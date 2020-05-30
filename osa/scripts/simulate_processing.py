e83742df6a50cf5358da9580baed3648d8d76c72
Jose Enrique Ruiz <jer@iaa.es>
2020-05-30 16:52:17 +0200

    parse built templates

--- osa/scripts/simulate_processing.py
+++ osa/scripts/simulate_processing.py
@@ -12,9 +12,25 @@ from lstchain.version import get_version
 BASE_PATH = Path(__file__).parent
 
 
-def parse_template(template):
+def parse_template(template, idx):
     """Parse batch templates."""
-    return template
+
+    args = []
+    keep = False
+    for line in template.splitlines():
+        if keep:
+            line = line.replace("'", "")
+            line = line.replace(",", "")
+            line = line.replace(r"{0}.format(str(subruns).zfill(4))", str(idx).zfill(4))
+            if "--stdout=" in line or "--stderr" in line or "srun" in line:
+                continue
+            if "--prod_id" in line:
+                args.append("-s")
+            args.append(line.strip())
+        if line.startswith("subprocess.call"):
+            keep = True
+    args.pop()
+    return args
 
 
 def run_simulate_processing():
@@ -37,9 +53,9 @@ def run_simulate_processing():
     # loop running sequentially
     # datasequence.py with args and option simulate to capture prov
     # provprocess.py with args producing prov in temp/physical folder
-    for s in sequence_list:
-        parsed_template = parse_template(createjobtemplate(s, get_content=True))
-        print(parsed_template)
+    for idx, s in enumerate(sequence_list):
+        args = parse_template(createjobtemplate(s, get_content=True), idx)
+        print(args)
 
 
 if __name__ == "__main__":
