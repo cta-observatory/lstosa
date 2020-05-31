15cc917204545710a095081245c97f75d3ca036b
Jose Enrique Ruiz <jer@iaa.es>
2020-05-31 13:16:49 +0200

    add loop in subruns and skip drs4/calibration runs

--- osa/scripts/simulate_processing.py
+++ osa/scripts/simulate_processing.py
@@ -1,15 +1,15 @@
 """
 Simulate executions of data processing pipeline and produce provenance
 """
+
 import logging
-from pathlib import Path
+import subprocess
 from osa.utils import options, cliopts
+from osa.utils.utils import lstdate_to_number
 from osa.jobs.job import createjobtemplate
 from osa.nightsummary.nightsummary import readnightsummary
 from osa.nightsummary import extract
 from lstchain.version import get_version
-
-BASE_PATH = Path(__file__).parent
 
 
 def parse_template(template, idx):
@@ -24,6 +24,8 @@ def parse_template(template, idx):
             line = line.replace(r"{0}.format(str(subruns).zfill(4))", str(idx).zfill(4))
             if "--stdout=" in line or "--stderr" in line or "srun" in line:
                 continue
+            # if "calibrationsequence.py" in line:
+            #     break
             if "--prod_id" in line:
                 args.append("-s")
             args.append(line.strip())
@@ -39,31 +41,42 @@ def run_simulate_processing():
     from osa.configs import config
 
     options.simulate = True
-    options.directory = BASE_PATH
     night_content = readnightsummary()
     logging.info(f"Night summary file content\n{night_content}")
 
+    options.directory = lstdate_to_number(options.date)
     sub_run_list = extract.extractsubruns(night_content)
     run_list = extract.extractruns(sub_run_list)
     sequence_list = extract.extractsequences(run_list)
     options.lstchain_version = 'v' + get_version()
     options.prod_id = options.lstchain_version + '_' + config.cfg.get('LST1', 'VERSION')
 
-    # TODO
-    # loop running sequentially
-    # datasequence.py with args and option simulate to capture prov
-    # provprocess.py with args producing prov in temp/physical folder
-    for idx, s in enumerate(sequence_list):
-        args = parse_template(createjobtemplate(s, get_content=True), idx)
-        print(args)
+    # skip drs4 and calibration
+    start_run_idx = 1
+    start_subrun_idx = 2
+    for run_idx, s in enumerate(sequence_list[start_run_idx:]):
+        for subrun_idx in range(sub_run_list[run_idx + start_subrun_idx].subrun):
+            args_ds = parse_template(createjobtemplate(s, get_content=True), subrun_idx)
+            subprocess.run(args_ds)
+        args_pp = [
+            'python',
+            'provprocess.py',
+            s.run_str,
+            options.directory,
+            options.prod_id,
+        ]
+        # TODO: produce prov if overwrite arg
+        # subprocess.run(args_pp)
+        # print(sub_run_list[run_idx + start_subrun_idx].subrun)
+        # print(args_pp)
 
 
 if __name__ == "__main__":
-
     format = "%(filename)s: %(message)s"
     logging.basicConfig(level=logging.INFO, format=format)
+
+    # TODO: make specific cli parsing
     cliopts.sequencercliparsing()
 
     logging.info(f"Running simulate processing")
-
     run_simulate_processing()
