ab83698c6a99f6c85eca4e1d47c73b3fd1be0dfc
Jose Enrique Ruiz <jer@iaa.es>
2020-06-16 18:24:23 +0200

    clean utils.py

--- osa/utils/utils.py
+++ osa/utils/utils.py
@@ -18,33 +18,33 @@ from .standardhandle import error, errornonfatal, gettag, verbose, warning
 def getdate(date, sep):
     tag = gettag()
     if not options.date:
-        stringdate = date.replace(cfg.get('LST', 'DATESEPARATOR'), sep)
+        stringdate = date.replace(cfg.get("LST", "DATESEPARATOR"), sep)
     else:
         stringdate = getcurrentdate2(sep)
-        verbose(tag, 'date is ' + stringdate)
+        verbose(tag, f"date is {stringdate}")
     return stringdate
 
 
 def getcurrentdate2(sep):
     tag = gettag()
-    limitnight = int(cfg.get('LST', 'NIGHTOFFSET'))
+    limitnight = int(cfg.get("LST", "NIGHTOFFSET"))
     now = datetime.utcnow()
     if (now.hour >= limitnight >= 0) or (now.hour < limitnight + 24 and limitnight < 0):
-        # Today, nothing to do
+        # today, nothing to do
         pass
     elif now.hour < limitnight and limitnight >= 0:
-        # Yesterday
+        # yesterday
         gap = timedelta(hours=24)
         now = now - gap
     elif now.hour >= limitnight + 24 and limitnight < 0:
-        # Tomorrow
+        # tomorrow
         gap = timedelta(hours=24)
         now = now + gap
     else:
-        error(tag, "ERROR: NIGHTOFFSET should be between range (-24, 24)\n")
+        error(tag, "ERROR: NIGHTOFFSET should be between range (-24, 24)\n", 2)
         sys.exit(4)
     stringdate = now.strftime("%Y" + sep + "%m" + sep + "%d")
-    verbose(tag, "stringdate by default " + stringdate)
+    verbose(tag, f"stringdate by default {stringdate}")
     return stringdate
 
 
@@ -55,18 +55,17 @@ def getnightdirectory():
 
     if not options.prod_id:
         import warnings
-        warnings.simplefilter(action='ignore', category=FutureWarning)
-        from lstchain.version import get_version
-        options.lstchain_version = 'v' + get_version()
-        options.prod_id = options.lstchain_version + '_' + cfg.get('LST1', 'VERSION')
 
-    directory = join(
-        cfg.get(options.tel_id, 'ANALYSISDIR'),
-        nightdir, options.prod_id
-    )
+        warnings.simplefilter(action="ignore", category=FutureWarning)
+        from lstchain.version import get_version
+
+        options.lstchain_version = "v" + get_version()
+        options.prod_id = options.lstchain_version + "_" + cfg.get("LST1", "VERSION")
+
+    directory = join(cfg.get(options.tel_id, "ANALYSISDIR"), nightdir, options.prod_id)
 
     if not exists(directory):
-        if options.nightsum and options.tel_id != 'ST':
+        if options.nightsum and options.tel_id != "ST":
             error(tag, f"Night directory {directory} does not exists!", 2)
         elif options.simulate:
             warning(tag, f"directory {directory} does not exists")
@@ -80,19 +79,16 @@ def make_directory(dir):
     tag = gettag()
     if exists(dir):
         if not isdir(dir):
-            # Ups!! a file instead of a dir?
+            # oups!! a file instead of a dir?
             error(tag, f"{dir} exists but is not a directory", 2)
         else:
-            # It is a directory, OK, we could check for access.
+            # it is a directory, OK, we could check for access.
             return False
     else:
         try:
             makedirs(dir)
         except IOError:
             error(tag, f"Problems creating {dir}, {IOError}", 2)
-        # except OSError as (ValueError, NameError):
-        except OSError:
-            error(tag, f"Problems creating {dir}, {OSError}", ValueError)
         else:
             verbose(tag, f"Created {dir}")
             return True
@@ -111,25 +107,24 @@ def sorted_nicely(l):
     -------
 
     """
-    tag = gettag()
     convert = lambda text: int(text) if text.isdigit() else text
-    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
     return sorted(l, key=alphanum_key)
 
 
 def getrawdatadays():
     tag = gettag()
-    daqdir = cfg.get(options.tel_id, 'RAWDIR')
+    daqdir = cfg.get(options.tel_id, "RAWDIR")
     validset = []
     try:
         dirs = os.listdir(daqdir)
     # except OSError as (ValueError, NameError):
     except OSError:
-        error(tag, f"{daqdir}, {OSError}", ValueError)
+        error(tag, f"{daqdir}, {OSError}", OSError)
     else:
         for element in dirs:
-            if isdir(join(daqdir, element)) and fnmatch(element, '20[0-9][0-9]_[0-1][0-9]_[0-3][0-9]'):
-                # We check for directory naming
+            if isdir(join(daqdir, element)) and fnmatch(element, "20[0-9][0-9]_[0-1][0-9]_[0-3][0-9]"):
+                # we check for directory naming
                 verbose(tag, f"Found raw dir {element}")
                 validset.append(element)
     return set(validset)
@@ -137,36 +132,30 @@ def getrawdatadays():
 
 def getstereodatadays():
     tag = gettag()
-    stereodir = cfg.get(options.tel_id, 'ANALYSISDIR')
+    stereodir = cfg.get(options.tel_id, "ANALYSISDIR")
     validset = set()
     for root, dirs, files in walk(stereodir):
         for element in dirs:
             month = basename(root)
             year = basename(dirname(root))
             if isdir(join(root, element)) and dirname(dirname(root)) == stereodir:
-                # We check for directory naming
-                verbose(
-                    tag,
-                    f"Found stereo dir {element}, {dirname(dirname(root))} == {stereodir}"
-                )
-                validset.add('_'.join([year, month, element]))
+                # we check for directory naming
+                verbose(tag, f"Found stereo dir {element}, {dirname(dirname(root))} == {stereodir}")
+                validset.add("_".join([year, month, element]))
             else:
-                verbose(
-                    tag,
-                    f"Element {element} not a stereo dir {isdir(element)}, {dirname(dirname(root))} != {stereodir}"
-                )
+                verbose(tag, f"Element {element} not a stereo dir {isdir(element)}, {dirname(dirname(root))} != {stereodir}")
     return validset
 
 
 def getfinisheddays():
     tag = gettag()
-    parent_lockdir = cfg.get(options.tel_id, 'CLOSERDIR')
-    basename = cfg.get('LSTOSA', 'ENDOFACTIVITYPREFIX') + cfg.get('LSTOSA', 'TEXTSUFFIX')
+    parent_lockdir = cfg.get(options.tel_id, "CLOSERDIR")
+    basename = cfg.get("LSTOSA", "ENDOFACTIVITYPREFIX") + cfg.get("LSTOSA", "TEXTSUFFIX")
     validlist = []
     for root, dirs, files in walk(parent_lockdir):
         for f in files:
             if f == basename:
-                # Got the day
+                # got the day
                 night_closed = dir_to_lstdate(root)
                 validlist.append(night_closed)
                 verbose(tag, f"Closed night {night_closed} found!")
@@ -181,7 +170,7 @@ def createlock(lockfile, content):
         return True
     else:
         if exists(lockfile) and isfile(lockfile):
-            with open(lockfile, 'r') as f:
+            with open(lockfile, "r") as f:
                 hostpid = f.readline()
             error(tag, f"Lock by a previous process {hostpid}, exiting!\n", 4)
         else:
@@ -201,8 +190,8 @@ def createlock(lockfile, content):
 
 def getlockfile():
     tag = gettag()
-    basename = cfg.get('LSTOSA', 'ENDOFACTIVITYPREFIX') + cfg.get('LSTOSA', 'TEXTSUFFIX')
-    dir = join(cfg.get(options.tel_id, 'CLOSERDIR'), lstdate_to_dir(options.date), options.prod_id)
+    basename = cfg.get("LSTOSA", "ENDOFACTIVITYPREFIX") + cfg.get("LSTOSA", "TEXTSUFFIX")
+    dir = join(cfg.get(options.tel_id, "CLOSERDIR"), lstdate_to_dir(options.date), options.prod_id)
     lockfile = join(dir, basename)
     verbose(tag, f"Lock file is {lockfile}")
     return lockfile
@@ -220,9 +209,8 @@ def lstdate_to_number(night):
     -------
 
     """
-    tag = gettag()
-    sepbar = ''
-    numberdate = night.replace(cfg.get('LST', 'DATESEPARATOR'), sepbar)
+    sepbar = ""
+    numberdate = night.replace(cfg.get("LST", "DATESEPARATOR"), sepbar)
     return numberdate
 
 
@@ -238,9 +226,8 @@ def lstdate_to_iso(night):
     -------
     Date in iso format YYYY-MM-DD
     """
-    tag = gettag()
-    sepbar = '-'
-    isodate = night.replace(cfg.get('LST', 'DATESEPARATOR'), sepbar)
+    sepbar = "-"
+    isodate = night.replace(cfg.get("LST", "DATESEPARATOR"), sepbar)
     return isodate
 
 
@@ -256,7 +243,7 @@ def lstdate_to_dir(night):
 
     """
     tag = gettag()
-    nightdir = night.split(cfg.get('LST', 'DATESEPARATOR'))
+    nightdir = night.split(cfg.get("LST", "DATESEPARATOR"))
     if len(nightdir) != 3:
         error(tag, f"Error: night directory structure could not be created from {nightdir}\n", 1)
     # dir = join(nightdir[0], nightdir[1], nightdir[2])
@@ -276,31 +263,27 @@ def dir_to_lstdate(dir):
 
     """
     tag = gettag()
-    sep = cfg.get('LST', 'DATESEPARATOR')
+    sep = cfg.get("LST", "DATESEPARATOR")
     dircopy = dir
-    nightdir = ['YYYY', 'MM', 'DD']
+    nightdir = ["YYYY", "MM", "DD"]
     for i in reversed(range(3)):
         dircopy, nightdir[i] = split(dircopy)
     night = sep.join(nightdir)
-
     if len(night) != 10:
         error(tag, f"Error: night {night} could not be created from {dir}\n", 1)
     return night
 
 
 def build_lstbasename(prefix, suffix):
-    tag = gettag()
     basename = f"{prefix}_{lstdate_to_number(options.date)}{suffix}"
     return basename
 
 
 def is_defined(variable):
-    tag = gettag()
     try:
         variable
     except NameError:
         variable = None
-
     if variable is not None:
         return True
     else:
@@ -309,15 +292,16 @@ def is_defined(variable):
 
 def get_night_limit_timestamp():
     from dev.mysql import select_db
+
     tag = gettag()
     night_limit = None
-    server = cfg.get('MYSQL', 'server')
-    user = cfg.get('MYSQL', 'user')
-    database = cfg.get('MYSQL', 'database')
-    table = cfg.get('MYSQL', 'nighttimes')
+    server = cfg.get("MYSQL", "server")
+    user = cfg.get("MYSQL", "user")
+    database = cfg.get("MYSQL", "database")
+    table = cfg.get("MYSQL", "nighttimes")
     night = lstdate_to_iso(options.date)
-    selections = ['END']
-    conditions = {'NIGHT': night}
+    selections = ["END"]
+    conditions = {"NIGHT": night}
     matrix = select_db(server, user, database, table, selections, conditions)
     if len(matrix) > 0:
         night_limit = matrix[0][0]
@@ -332,21 +316,21 @@ def get_md5sum_and_copy(inputf, outputf):
     md5 = hashlib.md5()
     outputdir = dirname(outputf)
     make_directory(outputdir)
-    # In case of being a link we just move it
+    # in case of being a link we just move it
     if islink(inputf):
         linkto = readlink(inputf)
         symlink(linkto, outputf)
         return None
     else:
         try:
-            with open(inputf, 'rb') as f, open(outputf, 'wb') as o:
+            with open(inputf, "rb") as f, open(outputf, "wb") as o:
                 block_size = 8192
-                for chunk in iter(lambda: f.read(128 * block_size), b''):
+                for chunk in iter(lambda: f.read(128 * block_size), b""):
                     md5.update(chunk)
-                    # Got this error: write() argument must be str, not bytes
+                    # got this error: write() argument must be str, not bytes
                     o.write(chunk)
         # except IOError as (ErrorValue, ErrorName):
         except IOError as ErrorValue:
-            error(tag, f"{ErrorValue}")
+            error(tag, f"{ErrorValue}", 2)
         else:
             return md5.hexdigest()
