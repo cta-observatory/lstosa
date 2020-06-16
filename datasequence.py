5c46a11e5279884d5bcd4577a1cf9200a4f560df
Jose Enrique Ruiz <jer@iaa.es>
2020-06-16 13:23:59 +0200

    clean datasequence.py

--- datasequence.py
+++ datasequence.py
@@ -10,10 +10,11 @@ from osa.utils import cliopts, options
 from osa.utils.standardhandle import error, gettag, verbose
 from osa.utils.utils import lstdate_to_dir
 
-__all__ = ["datasequence", "r0_to_dl1", 'dl1_to_dl2']
+__all__ = ["datasequence", "r0_to_dl1", "dl1_to_dl2"]
 
 
 def datasequence(args):
+
     calibrationfile = args[0]
     pedestalfile = args[1]
     time_calibration = args[2]
@@ -23,12 +24,10 @@ def datasequence(args):
     ucts_t0_tib = args[6]
     tib_counter0 = args[7]
     run_str = args[8]
-
-    historysuffix = cfg.get('LSTOSA', 'HISTORYSUFFIX')
-    sequenceprebuild = join(options.directory, f'sequence_{options.tel_id}_{run_str}')
+    historysuffix = cfg.get("LSTOSA", "HISTORYSUFFIX")
+    sequenceprebuild = join(options.directory, f"sequence_{options.tel_id}_{run_str}")
     historyfile = sequenceprebuild + historysuffix
-
-    level, rc = (3, 0) if options.simulate else historylevel(historyfile, 'DATA')
+    level, rc = (3, 0) if options.simulate else historylevel(historyfile, "DATA")
     verbose(tag, f"Going to level {level}")
 
     if level == 3:
@@ -42,7 +41,7 @@ def datasequence(args):
             ucts_t0_tib,
             tib_counter0,
             run_str,
-            historyfile
+            historyfile,
         )
         level -= 1
         verbose(tag, f"Going to level {level}")
@@ -69,7 +68,6 @@ def r0_to_dl1(
     run_str,
     historyfile,
 ):
-
     """Perform low and high-level calibration to raw camera images.
     Apply image cleaning and obtain shower parameters.
 
@@ -90,30 +88,27 @@ def r0_to_dl1(
     if options.simulate:
         return 0
 
-
-    configfile = cfg.get('LSTOSA', 'CONFIGFILE')
-    lstchaincommand = cfg.get('LSTOSA', 'R0-DL1')
+    configfile = cfg.get("LSTOSA", "CONFIGFILE")
+    lstchaincommand = cfg.get("LSTOSA", "R0-DL1")
     nightdir = lstdate_to_dir(options.date)
     fullcommand = lstchaincommand
     datafile = join(
-        cfg.get('LST1', 'RAWDIR'),
-        nightdir,
-        f'{cfg.get("LSTOSA", "R0PREFIX")}.Run{run_str}{cfg.get("LSTOSA", "R0SUFFIX")}'
+        cfg.get("LST1", "RAWDIR"), nightdir, f'{cfg.get("LSTOSA", "R0PREFIX")}.Run{run_str}{cfg.get("LSTOSA", "R0SUFFIX")}'
     )
 
     commandargs = [
         fullcommand,
-        '--input-file', datafile,
-        '--output-dir', options.directory,
-        '--pedestal-file', pedestalfile,
-        '--calibration-file', calibrationfile,
-        '--config', configfile,
-        '--time-calibration-file', time_calibration,
-        '--pointing-file', drivefile,
-        '--ucts-t0-dragon', ucts_t0_dragon,
-        '--dragon-counter0', dragon_counter0,
-        '--ucts-t0-tib', ucts_t0_tib,
-        '--tib-counter0', tib_counter0
+        "--input-file=" + datafile,
+        "--output-dir=" + options.directory,
+        "--pedestal-file=" + pedestalfile,
+        "--calibration-file=" + calibrationfile,
+        "--config=" + configfile,
+        "--time-calibration-file=" + time_calibration,
+        "--pointing-file=" + drivefile,
+        "--ucts-t0-dragon=" + ucts_t0_dragon,
+        "--dragon-counter0=" + dragon_counter0,
+        "--ucts-t0-tib=" + ucts_t0_tib,
+        "--tib-counter0=" + tib_counter0,
     ]
 
     try:
@@ -122,20 +117,15 @@ def r0_to_dl1(
     except subprocess.CalledProcessError as Error:
         error(tag, f"{Error}", rc)
     except OSError as ValueError:
-        error(tag, f"Command {'stringify(commandargs)'} failed, {NameError}", ValueError)
+        error(tag, f"Command {'stringify(commandargs)'} failed, {ValueError}", ValueError)
     else:
-        report.history(
-            run_str, basename(fullcommand),
-            basename(calibrationfile),
-            basename(pedestalfile),
-            rc, historyfile
-        )
+        report.history(run_str, basename(fullcommand), basename(calibrationfile), basename(pedestalfile), rc, historyfile)
         return rc
 
 
 @trace
 def dl1_to_dl2(run_str, historyfile):
-    """ Apply already trained RFs models to DL1 files.
+    """Apply already trained RFs models to DL1 files.
     It identifies the primary particle, reconstructs the energy
     and direction of the primary particle.
 
@@ -148,24 +138,21 @@ def dl1_to_dl2(run_str, historyfile):
     if options.simulate:
         return 0
 
-    configfile = cfg.get('LSTOSA', 'CONFIGFILE')
-    rf_models_directory = cfg.get('LSTOSA', 'RF-MODELS-DIR')
-    lstchaincommand = cfg.get('LSTOSA', 'DL1-DL2')
-    # nightdir = lstdate_to_dir(options.date)
+    configfile = cfg.get("LSTOSA", "CONFIGFILE")
+    rf_models_directory = cfg.get("LSTOSA", "RF-MODELS-DIR")
+    lstchaincommand = cfg.get("LSTOSA", "DL1-DL2")
     fullcommand = lstchaincommand
-    datafile = join(
-        options.directory,
-        f'{cfg.get("LSTOSA", "DL1PREFIX")}.Run{run_str}{cfg.get("LSTOSA", "DL1SUFFIX")}'
-    )
+    datafile = join(options.directory, f'{cfg.get("LSTOSA", "DL1PREFIX")}.Run{run_str}{cfg.get("LSTOSA", "DL1SUFFIX")}')
 
+    # nightdir = lstdate_to_dir(options.date)
     # dl2_directory = join(cfg.get('LST1', 'DL2DIR'), nightdir, options.prod_id)
 
     commandargs = [
         fullcommand,
-        '--input-file', datafile,
-        '--output-dir', options.directory,
-        '--path-models', rf_models_directory,
-        '--config', configfile
+        "--input-file=" + datafile,
+        "--output-dir=" + options.directory,
+        "--path-models=" + rf_models_directory,
+        "--config=" + configfile,
     ]
 
     try:
@@ -174,20 +161,17 @@ def dl1_to_dl2(run_str, historyfile):
     except subprocess.CalledProcessError as Error:
         error(tag, f"{Error}", rc)
     except OSError as ValueError:
-        error(tag, f"Command {'stringify(commandargs)'} failed, {NameError}", ValueError)
+        error(tag, f"Command {'stringify(commandargs)'} failed, {ValueError}", ValueError)
     else:
-        report.history(
-            run_str, basename(fullcommand),
-            basename(datafile), basename(configfile),
-            rc, historyfile
-        )
+        report.history(run_str, basename(fullcommand), basename(datafile), basename(configfile), rc, historyfile)
         return rc
 
 
 if __name__ == "__main__":
+
     tag = gettag()
-    # Set the options through cli parsing
+    # set the options through cli parsing
     args = cliopts.datasequencecliparsing(sys.argv[0])
-    # Run the routine
+    # run the routine
     rc = datasequence(args)
     sys.exit(rc)
