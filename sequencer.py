66b12144b4075f518a021e3e42597fa442b50916
Jose Enrique Ruiz <jer@iaa.es>
2020-09-11 13:27:54 +0200

    move closer.py to autocloser folder

--- sequencer.py
+++ sequencer.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 from glob import glob
 from os.path import join
 
-from closer import is_day_closed
+from osa.autocloser.closer import is_day_closed
 from dev.dot import writeworkflow
 from osa.configs.config import cfg
 from osa.jobs.job import getqueuejoblist, preparedailyjobs, preparejobs, preparestereojobs, submitjobs
