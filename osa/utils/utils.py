00ad1c7cafa8ae17d9c73852b29a618c8d314722
Jose Enrique Ruiz <jer@iaa.es>
2020-09-18 14:42:52 +0200

    move is_day_closed function to utils.py

--- osa/utils/utils.py
+++ osa/utils/utils.py
@@ -334,3 +334,13 @@ def get_md5sum_and_copy(inputf, outputf):
             error(tag, f"{ErrorValue}", 2)
         else:
             return md5.hexdigest()
+
+
+def is_day_closed():
+    """Get the name and Check for the existence of the Closer flag file."""
+
+    answer = False
+    flag_file = getlockfile()
+    if exists(flag_file):
+        answer = True
+    return answer
