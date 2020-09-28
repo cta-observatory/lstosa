d6c4deee6d9aa6e9dc6a5f386d0e23685df5a0ed
Daniel Morcuende <dmorcuen@ucm.es>
2020-09-28 16:56:59 +0200

    prod_id from cfg if given, otherwise from lstchain version

--- osa/utils/utils.py
+++ osa/utils/utils.py
@@ -59,9 +59,12 @@ def getnightdirectory():
         warnings.simplefilter(action="ignore", category=FutureWarning)
         from lstchain.version import get_version
 
-        #options.lstchain_version = "v" + get_version()
-        #options.prod_id = options.lstchain_version + "_" + cfg.get("LST1", "VERSION")
-        options.prod_id = cfg.get("LST1", "DL1-PROD-ID")
+        options.lstchain_version = "v" + get_version()
+
+        if cfg.get("LST1", "DL1-PROD-ID") is not None:
+            options.prod_id = cfg.get("LST1", "DL1-PROD-ID")
+        else:
+            options.prod_id = options.lstchain_version + "_" + cfg.get("LST1", "VERSION")
 
     directory = join(cfg.get(options.tel_id, "ANALYSISDIR"), nightdir, options.prod_id)
 
