1995d8e58c6a20c79976c867a3a67a0af8f2eccf
Daniel Morcuende <dmorcuen@ucm.es>
2021-01-06 21:10:57 +0100

    keep autoapi for now

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,6 @@ copyright = f"{author}.  Last updated {datetime.datetime.now().strftime('%d %b %
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -39,10 +38,10 @@ extensions = [
     'sphinx_automodapi.automodapi',
     'sphinxarg.ext',
     'sphinx.ext.napoleon',
-    'autoapi.extension'
+    # 'autoapi.extension'
 ]
 
-autoapi_dirs = ['../osa']
+# autoapi_dirs = ['../osa']
 
 numpydoc_show_class_members = False
 
@@ -53,7 +52,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
