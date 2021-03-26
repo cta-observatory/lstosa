1e34e9ad505ca612d52c28949aefdee49942aa46
Daniel Morcuende <dmorcuen@ucm.es>
2021-03-26 13:49:33 +0100

    black precommit formatting

--- osa/conftest.py
+++ osa/conftest.py
@@ -7,5 +7,5 @@ import pytest
 @pytest.fixture(scope="session")
 def temp_dir():
     """Shared temporal directory for the tests."""
-    with tempfile.TemporaryDirectory(prefix='test_lstchain') as d:
+    with tempfile.TemporaryDirectory(prefix="test_lstchain") as d:
         yield Path(d)
