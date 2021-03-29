b81cb39885520dd0ad31b02becc055ac82f19b4a
Daniel Morcuende <dmorcuen@ucm.es>
2021-03-29 10:46:06 +0200

    create dir and analysis test files in conftest

--- osa/conftest.py
+++ osa/conftest.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from pathlib import Path
 
@@ -13,6 +14,38 @@ def temp_dir():
     """Shared temporal directory for the tests."""
     with tempfile.TemporaryDirectory(prefix="test_osa") as d:
         yield Path(d)
+
+
+@pytest.fixture(scope="session")
+def test_data():
+    """
+    Create dummy test directory and files to test closer
+    """
+    test_dir = "testfiles"
+    date = "20200117"
+    prod_id = "v0.1.0_01"
+    dl1_prod_id = "tailcut84"
+
+    running_analysis = os.path.join(test_dir, "running_analysis", date, prod_id)
+    dl1ab_directory = os.path.join(running_analysis, dl1_prod_id)
+
+    os.makedirs(test_dir, exist_ok=True)
+    os.makedirs(running_analysis, exist_ok=True)
+    os.makedirs(dl1ab_directory, exist_ok=True)
+
+    # Dummy files in running_analysis directory
+    fd, dl1_file = tempfile.mkstemp(prefix="dl1_", suffix=".h5", dir=running_analysis)
+    fd, dl1ab_file = tempfile.mkstemp(prefix="dl1_", suffix=".h5", dir=dl1ab_directory)
+    fd, dl2_file = tempfile.mkstemp(prefix="dl2_", suffix=".h5", dir=running_analysis)
+    fd, muons_file = tempfile.mkstemp(prefix="muons_", suffix=".fits", dir=running_analysis)
+    fd, dcheck_file = tempfile.mkstemp(prefix="datacheck_dl1_", suffix=".h5", dir=dl1ab_directory)
+    fd, calib_file = tempfile.mkstemp(prefix="calibration_", suffix=".hdf5", dir=running_analysis)
+    fd, drs4_file = tempfile.mkstemp(prefix="drs4_", suffix=".fits", dir=running_analysis)
+    fd, time_file = tempfile.mkstemp(
+        prefix="time_calibration_", suffix=".hdf5", dir=running_analysis
+    )
+
+    return test_dir, dl1_file
 
 
 @pytest.fixture(scope="session")
