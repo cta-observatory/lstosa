5f155c42663ba61a7db894b53159fcf61473abae
Daniel Morcuende <dmorcuen@ucm.es>
2021-03-29 10:45:19 +0200

    add general test osa to cleanup test analysis products

---
+++ osa/tests/test_osa.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import tempfile
+from pathlib import Path
+import shutil
+import re
+import glob
+import pytest
+
+
+@pytest.mark.firt
+def test_check_test_files(test_data):
+    """
+    Function to clean the test files created by the previous test
+    """
+    assert os.path.exists(test_data[0])  # Directory
+    assert os.path.exists(test_data[1])  # Directory
+
+
+@pytest.mark.last
+def test_clean_test_files(test_data):
+    """
+    Function to clean the test files created by the previous test
+    """
+    import shutil
+
+    shutil.rmtree(test_data[0])
+    # FIXME: produce this prov.log file inside
+    if os.path.exists("prov.log"):
+        os.remove("prov.log")
