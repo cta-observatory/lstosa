5dfd1de8ac2d6df371011c74b3d97f75494a2dbf
Daniel Morcuende <dmorcuen@ucm.es>
2021-03-30 18:14:29 +0200

    test preparejobs

--- osa/jobs/tests/test_jobs.py
+++ osa/jobs/tests/test_jobs.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 from osa.configs import options
 
-test_data = Path(os.getenv("OSA_TEST_DATA", "extra"))
-datasequence_history_file = test_data / "history_files/sequence_LST1_04185.0010.history"
-calibration_history_file = test_data / "history_files/sequence_LST1_04183.history"
+extra_files = Path(os.getenv("OSA_TEST_DATA", "extra"))
+datasequence_history_file = extra_files / "history_files/sequence_LST1_04185.0010.history"
+calibration_history_file = extra_files / "history_files/sequence_LST1_04183.history"
+options.date = "2020_01_17"
+options.tel_id = "LST1"
 
 
 def test_historylevel():
@@ -30,16 +32,21 @@ def test_historylevel():
     assert rc == 0
 
 
-def test_createjobtemplate(sequence_list):
-    from osa.jobs.job import createjobtemplate
-
-    options.simulate = True
-    for sequence in sequence_list:
-        createjobtemplate(sequence)
-
-
-def test_preparejobs(sequence_list):
+def test_preparejobs(test_data, sequence_list):
     from osa.jobs.job import preparejobs
 
-    options.simulate = True
+    options.simulate = False
+    options.directory = test_data[3]
     preparejobs(sequence_list)
+    expected_calib_script = os.path.join(test_data[3], "sequence_LST1_01805.py")
+    expected_data_script = os.path.join(test_data[3], "sequence_LST1_01807.py")
+    assert os.path.isfile(os.path.abspath(expected_calib_script))
+    assert os.path.isfile(os.path.abspath(expected_data_script))
+
+
+def test_setsequencefilenames(test_data, sequence_list):
+    from osa.jobs.job import setsequencefilenames
+
+    for sequence in sequence_list:
+        setsequencefilenames(sequence)
+        assert sequence.script == os.path.join(test_data[3], f"sequence_LST1_{sequence.run:05d}.py")
