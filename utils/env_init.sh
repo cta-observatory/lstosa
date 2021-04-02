1e4ee0681afdf125c1158d12f24835318f765c74
Daniel Morcuende <dmorcuen@ucm.es>
2021-04-02 16:46:30 +0200

    typo

--- utils/env_init.sh
+++ utils/env_init.sh
@@ -1,5 +1,5 @@
 # Set in a unique way the cache and config directories for the python packages
-# involved in the processing. Right these are matplotlib, astropy and ctapipe.
+# involved in the processing. These are matplotlib, astropy and ctapipe.
 export XDG_CACHE_HOME="/fefs/aswg/lstanalyzer/"
 export XDG_CONFIG_HOME="/fefs/aswg/lstanalyzer/"
 export CTAPIPE_SVC_PATH="/fefs/aswg/lstanalyzer/.ctapipe/service"
