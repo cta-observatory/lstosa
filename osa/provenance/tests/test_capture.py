e9cbbc3fc762b7bc1214ddcc17f0f566f06aea0a
Daniel Morcuende <dmorcuen@ucm.es>
2021-11-25 20:19:00 +0100

    test prov files are created

--- osa/provenance/tests/test_capture.py
+++ osa/provenance/tests/test_capture.py
@@ -3,27 +3,28 @@ import sys
 from pathlib import Path, PurePath
 
 from osa.configs import options
+from osa.configs.config import cfg
 from osa.provenance.capture import trace
 from osa.provenance.io import read_prov, provlist2provdoc, provdoc2json, provdoc2graph
 
 
 @trace
 def r0_to_dl1(
-    calibrationfile,
-    pedestalfile,
-    time_calibration,
-    drivefile,
-    runsummary,
-    run_str,
-    historyfile,
+    calibrationfile: Path,
+    pedestalfile: Path,
+    time_calibration: Path,
+    drivefile: Path,
+    runsummary: Path,
+    run_str: str,
+    historyfile: Path,
 ):
     pass
 
 
 @trace
 def dl1_to_dl2(
-    run_str,
-    historyfile,
+    run_str: str,
+    historyfile: Path,
 ):
     pass
 
@@ -48,13 +49,14 @@ def select_config(tmp_path):
 def make_args_r0_to_dl1():
 
     return (
-        "calibration.Run02006.0000.hdf5",
-        "drs4_pedestal.Run02005.0000.fits",
-        "time_calibration",
-        "drivefile",
-        "RunSummary_20200101.ecsv",
+        Path("calibration.Run02006.0000.hdf5"),
+        Path("drs4_pedestal.Run02005.0000.fits"),
+        Path("time_calibration"),
+        Path("drivefile"),
+        Path("RunSummary_20200101.ecsv"),
         "02006.0002",
-        "/fefs/aswg/data/real/running_analysis/20200218/v0.4.3_v00/sequence_LST1_02006.0000.history",
+        Path("/fefs/aswg/data/real/running_analysis/20200218/"
+             "v0.4.3_v00/sequence_LST1_02006.0000.history"),
     )
 
 
@@ -62,7 +64,8 @@ def make_args_dl1_to_dl2():
 
     return (
         "02006.0002",
-        "/fefs/aswg/data/real/running_analysis/20200218/v0.4.3_v00/sequence_LST1_02006.0000.history",
+        Path("/fefs/aswg/data/real/running_analysis/20200218/"
+             "v0.4.3_v00/sequence_LST1_02006.0000.history"),
     )
 
 
@@ -76,22 +79,29 @@ def test_trace_r0_to_dl2(tmp_path):
     args_dl2 = make_args_dl1_to_dl2()
 
     options.date = "2020_01_17"
-    options.calib_prod_id = "v01"
-    options.dl2_prod_id = "v0.1.0_v01"
-    options.prod_id = "v0.1.0"
+    options.prod_id = cfg.get("LST1", "PROD_ID")
+    options.calib_prod_id = cfg.get("LST1", "CALIB_PROD_ID")
+    options.dl1_prod_id = cfg.get("LST1", "DL1_PROD_ID")
+    options.dl2_prod_id = cfg.get("LST1", "DL2_PROD_ID")
 
     # track prov
     r0_to_dl1(*args_dl1)
     dl1_to_dl2(*args_dl2)
 
+    prov_filepath = path_logfile / "prov.log"
+
     # make json
     json_filepath = tmp_path / "prov.json"
-    provdoc = provlist2provdoc(read_prov(filename=path_logfile / "prov.log"))
+    provdoc = provlist2provdoc(read_prov(filename=prov_filepath))
     provdoc2json(provdoc, str(json_filepath))
 
     # make graph
-    png_filepath = tmp_path / "prov.pdf"
-    provdoc2graph(provdoc, str(png_filepath), "pdf")
+    pdf_filepath = tmp_path / "prov.pdf"
+    provdoc2graph(provdoc, str(pdf_filepath), "pdf")
+
+    assert prov_filepath.exists()
+    assert json_filepath.exists()
+    assert pdf_filepath.exists()
 
     try:
         Path(path_logfile / "prov.log").unlink()
