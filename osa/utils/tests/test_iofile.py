a2e2e63638a63530b5b38d0110d5472c508324ea
Daniel Morcuende <dmorcuen@ucm.es>
2021-12-22 19:20:26 +0100

    properly test write and append IO functions

--- osa/utils/tests/test_iofile.py
+++ osa/utils/tests/test_iofile.py
@@ -1,12 +1,11 @@
 from osa.utils.iofile import append_to_file
-from pathlib import Path
+from osa.configs import options
 
 
-def test_write_to_file(txt_file_test):
+def test_write_and_append_to_file(txt_file_test):
     assert txt_file_test.exists()
-    assert isinstance(txt_file_test, Path)
-
-
-def test_append_to_file(txt_file_test):
+    assert txt_file_test.read_text() == 'This is a test'
+    options.simulate = False
     append_to_file(txt_file_test, '\nAnother line')
+    options.simulate = True
     assert txt_file_test.read_text() == 'This is a test\nAnother line'
