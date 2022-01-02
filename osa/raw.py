94f1b4fdaf1d5d0cddf870d0642b82aabbdfe77b
Daniel Morcuende <dmorcuen@ucm.es>
2022-01-02 15:49:51 +0100

    add some test for raw module

--- osa/raw.py
+++ osa/raw.py
@@ -8,40 +8,10 @@ from osa.utils.utils import lstdate_to_dir
 log = logging.getLogger(__name__)
 
 __all__ = [
-    "are_raw_files_transferred",
-    "are_raw_files_transferred_for_tel",
     "get_check_raw_dir",
     "get_raw_dir",
     "is_raw_data_available"
 ]
-
-
-def are_raw_files_transferred_for_tel(tel_id):
-    """Check that there is a flag indicating the end of transferring raw data."""
-    # TODO: This function is to be removed. The raw files are not being transferred
-    #  to RAID. We should check for the end of observation instead (around 8:00 UTC)
-    night_dir = lstdate_to_dir(options.date)
-    end_of_transfer = Path(cfg.get(tel_id, "ENDOFRAWTRANSFERDIR")) / night_dir
-    flag_file = end_of_transfer / cfg.get("LSTOSA", "end_of_activity")
-
-    if flag_file.exists():
-        log.info(f"Files for {options.date} {tel_id} are completely transferred to raid")
-        return True
-    else:
-        log.warning(f"File {flag_file} not found!")
-        log.info(
-            f"Files for {options.date} {tel_id} are not yet "
-            f"transferred to raid. Expecting more raw data"
-        )
-        return False
-
-
-def are_raw_files_transferred() -> bool:
-    """Return True if raw files are transferred to RAID."""
-    if options.tel_id != "ST":
-        return are_raw_files_transferred_for_tel(options.tel_id)
-
-    return False
 
 
 def get_check_raw_dir() -> Path:
---
+++ osa/tests/test_raw.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import pytest
+
+from osa.configs import options
+from osa.configs.config import cfg
+from osa.utils.utils import lstdate_to_dir
+
+
+def test_get_raw_dir():
+    options.date = "2020_01_17"
+    night_dir = lstdate_to_dir(options.date)
+    r0_dir = Path(cfg.get("LST1", "R0_DIR")) / night_dir
+    from osa.raw import get_raw_dir
+    assert get_raw_dir() == r0_dir
+
+
+def test_get_check_raw_dir(r0_dir):
+    options.date = "2020_01_18"
+    from osa.raw import get_check_raw_dir
+
+    with pytest.raises(OSError):
+        assert get_check_raw_dir()
+
+    options.date = "2020_01_17"
+    raw_dir = get_check_raw_dir()
+    assert raw_dir.resolve() == r0_dir
