322e23d352c3638fdec492c9d74ce8bbda849609
Daniel Morcuende <dmorcuen@ucm.es>
2022-01-02 15:50:34 +0100

    function to produce run summary file

--- osa/nightsummary/nightsummary.py
+++ osa/nightsummary/nightsummary.py
@@ -1,61 +1,50 @@
-"""Searches for the run summary file and reads it."""
+"""Handle the creation and reading of the run summary files."""
 
 import logging
 import subprocess
-import sys
 from pathlib import Path
 
 from astropy.table import Table
 
 from osa.configs.config import cfg
-from osa.raw import are_raw_files_transferred
 from osa.utils.utils import lstdate_to_dir, stringify
 
-__all__ = ["build_external", "get_runsummary_file", "run_summary_table"]
+__all__ = ["produce_run_summary_file", "get_run_summary_file", "run_summary_table"]
 
 log = logging.getLogger(__name__)
 
 
-def build_external(command, rawdir):
+def produce_run_summary_file(date: str) -> None:
     """
-    Calls the create run summary script.
+    Produce the run summary using the lstchain script.
 
     Parameters
     ----------
-    command : str
-        The command passed on to the subprocess call
-    rawdir : str
-        Passed on as argument to the command str.
-
-    Returns
-    -------
-    stdout : str
-        The output of the create nightsummary script.
+    date : str
+        Date in the format YYYY_MM_DD
     """
-    commandargs = [command]
-    if not are_raw_files_transferred():
-        # ask for an incomplete night summary
-        commandargs.append("-i")
-    commandargs.append(rawdir)
+    nightdir = lstdate_to_dir(date)
+    r0_dir = Path(cfg.get("LST1", "R0_DIR"))
+    run_summary_dir = Path(cfg.get("LST1", "RUN_SUMMARY_DIR"))
+
+    command = [
+        "lstchain_create_run_summary",
+        f"--date={nightdir}",
+        f"--r0-path={r0_dir}",
+        f"--output-dir={run_summary_dir}"
+    ]
+
     try:
-        stdout = subprocess.check_output(
-            commandargs,
-            universal_newlines=True,
-            shell=False
-        )
+        subprocess.run(command, check=True)
     except OSError as error:
-        log.exception(f"Command {stringify(commandargs)}, error: {error}")
+        log.exception(f"Command {stringify(command)}, error: {error}")
     except subprocess.CalledProcessError as error:
         log.exception(f"Subprocess error: {error}")
-    else:
-        log.debug(f"Getting output...{stdout.rstrip()}")
-        return stdout
 
 
-def run_summary_table(date) -> Table:
+def run_summary_table(date: str) -> Table:
     """
-    Reads the run summary ECSV file containing an astropy
-    Table with the following content:
+    Reads the run summary ECSV file containing as a Table with the following content:
      - run_id, datatype: int64
      - n_subruns, datatype: int64
      - run_type, datatype: string
@@ -67,29 +56,40 @@ def run_summary_table(date) -> Table:
      - dragon_reference_counter, datatype: uint64
      - dragon_reference_source, datatype: string
 
+    Parameters
+    ----------
+    date : str
+        Date in the format YYYY_MM_DD
+
     Returns
     -------
     table : astropy.Table
         Table with the content of the run summary ECSV file.
     """
-    night_summary_file = get_runsummary_file(date)
+    night_summary_file = get_run_summary_file(date)
     log.debug(f"Looking for run summary file {night_summary_file}")
+
     if not night_summary_file.exists():
-        log.error(f"Run summary file {night_summary_file} not found")
-        sys.exit(1)
+        log.info(f"Run summary file {night_summary_file} not found. Producing it.")
+        produce_run_summary_file(date)
 
     table = Table.read(night_summary_file)
     table.add_index(["run_id"])
     return table
 
 
-def get_runsummary_file(date) -> Path:
+def get_run_summary_file(date: str) -> Path:
     """
     Builds the file name of the run summary ECSV file.
 
+    Parameters
+    ----------
+    date : str
+        Date in the format YYYY_MM_DD
+
     Returns
     -------
-    runsummary_file : pathlib.Path
+    run_summary_file : pathlib.Path
         File name of the run summary ECSV file
     """
     nightdir = lstdate_to_dir(date)
