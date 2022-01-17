7f690dfa75299b21b1b46dee4effc5b3071c3235
Daniel Morcuende <dmorcuen@ucm.es>
2022-01-17 12:09:43 -0700

    check for database connection

--- osa/nightsummary/database.py
+++ osa/nightsummary/database.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 __all__ = ['query']
 
@@ -18,10 +19,25 @@ def query(obs_id: int, property_name: str):
     property_name : str
         Properties from drive information e.g. `DriveControl_SourceName`,
         `DriveControl_RA_Target`, `DriveControl_Dec_Target`
+
+    Returns
+    -------
+    query_result : str or None
+        Query result from database. It can be either the source name or its coordinates.
+
+    Raises
+    ------
+    ConnectionFailure
     """
 
     caco_client = MongoClient("tcs01")
     tcu_client = MongoClient("tcs05")
+
+    try:
+        caco_client.admin.command('ping')
+        tcu_client.admin.command('ping')
+    except ConnectionFailure:
+        raise ConnectionFailure("Databases not available")
 
     with caco_client, tcu_client:
         run_info = caco_client["CACO"]["RUN_INFORMATION"]
