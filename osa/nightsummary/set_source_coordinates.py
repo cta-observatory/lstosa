c13bd7fb62f2faf2412ec16ff7152527abc2cb38
Daniel Morcuende <dmorcuen@ucm.es>
2022-01-30 03:43:16 -0700

    add type hinting and default values

--- osa/nightsummary/set_source_coordinates.py
+++ osa/nightsummary/set_source_coordinates.py
@@ -1,8 +1,11 @@
+"""Script to update the source coordinates in the RunCatalog ECSV files."""
+
 import logging
+from pathlib import Path
 
 import click
-from astropy.coordinates import SkyCoord
-from astropy.coordinates import name_resolve
+import numpy as np
+from astropy.coordinates import SkyCoord, name_resolve
 from astropy.table import Table
 
 from osa.utils.logging import myLogger
@@ -15,14 +18,19 @@ log = myLogger(logging.getLogger(__name__))
 @click.option('--source', type=str)
 @click.option('--ra', type=float, help='Right Ascension in degrees')
 @click.option('--dec', type=float, help='Declination in degrees')
-def main(file, source, ra, dec):
-
+def main(
+        file: Path = None,
+        source: str = None,
+        ra: float = np.nan,
+        dec: float = np.nan
+):
+    """Update the source coordinates in the RunCatalog ECSV files."""
     log.setLevel(logging.INFO)
 
     table = Table.read(file)
     table.add_index('run_id')
 
-    for i, run in enumerate(table):
+    for run in table:
         source_name = run['source_name']
         try:
             coords = SkyCoord.from_name(source_name)
