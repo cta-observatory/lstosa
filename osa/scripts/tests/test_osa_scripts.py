a48235f399ce83b44b0d8fd809151fcd424e2a12
Jose Enrique Ruiz <jer@iaa.es>
2022-02-10 18:19:26 +0100

    fix tests

--- osa/scripts/tests/test_osa_scripts.py
+++ osa/scripts/tests/test_osa_scripts.py
@@ -90,16 +90,16 @@ def test_simulate_processing(
 
     with open(json_file_dl1) as file:
         dl1 = yaml.safe_load(file)
-    assert len(dl1["entity"]) == 26
+    assert len(dl1["entity"]) == 16
     assert len(dl1["activity"]) == 4
-    assert len(dl1["used"]) == 23
+    assert len(dl1["used"]) == 13
     assert len(dl1["wasGeneratedBy"]) == 7
 
     with open(json_file_dl2) as file:
         dl2 = yaml.safe_load(file)
-    assert len(dl2["entity"]) == 35
+    assert len(dl2["entity"]) == 25
     assert len(dl2["activity"]) == 6
-    assert len(dl2["used"]) == 31
+    assert len(dl2["used"]) == 21
     assert len(dl2["wasGeneratedBy"]) == 12
 
     rc = run_program("simulate_processing", "-p")
