900191d04eca8c38aa509e11ff21f72e68331992
Jose Enrique Ruiz <jer@iaa.es>
2022-02-10 18:10:11 +0100

    add InterleavedPedestalEventsFile

--- osa/provenance/utils.py
+++ osa/provenance/utils.py
@@ -98,7 +98,7 @@ def parse_variables(class_instance):
         # systematic_corr    [3] .../20200101/pro/no_sys_corrected_calibration_scan_fit_20210514.0000.h5
         # drive_file         [4] .../DrivePositioning/drive_log_20_02_18.txt
         # run_summary_file   [5] .../RunSummary/RunSummary_20200101.ecsv
-        # pedestal_ids_file  [6] .../RunSummary/RunSummary_20200101.ecsv
+        # pedestal_ids_file  [6] .../path/to/interleaved/pedestal/events.h5
         # run_str            [7] 02006.0000
 
         run_subrun_id = class_instance.args[7]
@@ -123,6 +123,9 @@ def parse_variables(class_instance):
         class_instance.MuonsSubrunDataset = os.path.realpath(
             f"{muon_dir}/muons_LST-1.Run{run_subrun_id}.fits"
         )
+        class_instance.InterleavedPedestalEventsFile = None
+        if class_instance.args[6] is not None:
+            class_instance.InterleavedPedestalEventsFile = os.path.realpath(class_instance.args[6])
 
     if class_instance.__name__ == "dl1ab":
         # run_str       [0] 02006.0000
