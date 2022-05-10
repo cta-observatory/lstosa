8010614599d25ea4ea065107b7c4b8638b25d56a
Maria Lainez <marialainez@up.gae.ucm.es>
2022-05-10 11:46:00 +0200

    convert date to a string

--- osa/paths.py
+++ osa/paths.py
@@ -74,7 +74,7 @@ def get_run_date(run_id: int) -> str:
         date_string = summary_table[summary_table["run_id"] == run_id]["date"][0]
     except IndexError:
         log.warning(f"Run {str(run_id)} is not in the summary table. Assuming the date of the run is {options.date}.")
-        date_string = options.date
+        date_string = str(options.date)
 
     return date_string.replace("-", "")
 
