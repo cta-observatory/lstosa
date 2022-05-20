e1c9be72eca18da72a3fb1eeaa62871a94bdb370
Daniel Morcuende <dmorcuen@ucm.es>
2022-05-20 08:39:25 +0200

    refactor webmaker, fixing some bugs related to its cli

--- osa/scripts/sequencer_webmaker.py
+++ osa/scripts/sequencer_webmaker.py
@@ -1,177 +1,166 @@
-import datetime
+"""Produce the HTML file with the processing status from the sequencer report."""
+
+
 import logging
 import subprocess as sp
 import sys
+from datetime import datetime, timedelta
 from pathlib import Path
+from textwrap import dedent
+from typing import Iterable
+
+import pandas as pd
 
 from osa.configs import options
+from osa.configs.config import cfg
 from osa.utils.cliopts import sequencer_webmaker_argparser
 from osa.utils.logging import myLogger
-from osa.utils.utils import is_day_closed, date_to_iso
+from osa.utils.utils import is_day_closed, date_to_iso, date_to_dir
 
 log = myLogger(logging.getLogger())
 
 
-def matrix_to_html_table(matrix, column_class, header, footer):
+def html_content(body: str, date: str) -> str:
+    """Build the HTML content.
+
+    Parameters
+    ----------
+    body : str
+        Table with the sequencer status report.
+    date : str
+        Date of the processing YYYY-MM-DD.
+
+    Returns
+    -------
+    str
+        HTML content.
     """
-    Header and footer are simple simple bool in order to build first and last line
-    with outside the body with th, td. Column_class is a css class to align columns.
-    """
+    time_update = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
-    is_tbody_open = False
-    is_tbody_closed = False
-    print("<table>")
-    for index, row in enumerate(matrix):
-        if index == 0 and header:
-            print("<thead>")
-        elif index == len(matrix) - 1 and footer:
-            print("<tfoot>")
-        elif not is_tbody_open:
-            print("<tbody>")
-            is_tbody_open = True
-        print("<tr>")
-        for col in row:
-            if (index == 0 and header) or row[0] == matrix[0][0]:
-                # I know this invalidates html, but it is nice some headers in between
-                print(
-                    '<th class="{1}">{0}</th>'.format(col, column_class[row.index(col)])
-                )
-            else:
-                print(
-                    '<td class="{1}">{0}</td>'.format(col, column_class[row.index(col)])
-                )
-        print("</tr>")
-        if index == 0 and header:
-            print("</thead>")
-        elif index == len(matrix) - 1 and footer:
-            print("</tfoot>")
-        elif not is_tbody_closed:
-            if index == len(matrix) - 1 or index == len(matrix) - 2 and footer:
-                print("</tbody>")
-                is_tbody_closed = True
-    print("</table>")
-
-
-def webhead(header):
-    print(
-        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"'
-        '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
-    )
-    print('<html xmlns="http://www.w3.org/1999/xhtml">')
-    print(" <head>")
-    print('  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />')
-    print(header)
-    print(" </head>")
-    print(" <body>")
-
-
-def webtail():
-    print(" </body>")
-    print("</html>")
-
-
-def main():
-
-    log.setLevel(logging.INFO)
-
-    args = sequencer_webmaker_argparser().parse_args()
-
-    if args.date:
-        options.date = args.date
-        flat_date = args.date.strftime("%Y%m%d")
-    else:
-        # yesterday by default
-        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-        options.date = yesterday.strftime("%Y_%m_%d")
-        flat_date = yesterday.strftime("%Y%m%d")
-
-    if is_day_closed():
-        log.info(
-            f"Date {date_to_iso(options.date)} is already closed for {options.tel_id}"
-        )
-        sys.exit(1)
-
-    run_summary_directory = Path("/fefs/aswg/data/real/monitoring/RunSummary")
-    run_summary_file = run_summary_directory / f"RunSummary_{flat_date}.ecsv"
-
-    if not run_summary_file.is_file():
-        print(f"No RunSummary file found for {date_to_iso(options.date)}")
-        sys.exit(1)
-
-    # Print the output into a web page
-    webhead(
-        '<title>OSA Sequencer in the LST-1 onsite IT center</title><link href="osa.css" '
-        'rel="stylesheet" type="text/css" /><style>table{width:152ex;}</style>'
+    return dedent(
+        f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+         <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <title>OSA Sequencer status</title><link href="osa.css" rel="stylesheet"
+          type="text/css" /><style>table{{width:152ex;}}</style>
+         </head>
+         <body>
+         <h1>OSA processing status</h1>
+         <p>Processing data from: {date}. Last updated: {time_update} UTC</p>
+         {body}
+         </body>
+        </html>"""
     )
 
-    print("<h1>OSA sequencer in the LST onsite IT center</h1>")
 
-    # Print the matrix
-    column_class = [
-        "left",
-        "right",
-        "right",
-        "left",
-        "left",
-        "left",
-        "left",
-        "left",
-        "right",
-        "left",
-        "left",
-        "left",
-        "left",
-        "right",
-        "right",
-        "right",
-        "right",
-        "right",
-    ]
+def get_sequencer_output(date: str, config: str, test=False) -> list:
+    """Call sequencer to get table with the sequencer status report.
 
-    #  Call sequencer
+    Parameters
+    ----------
+    date : str
+        Date of the processing YYYY-MM-DD.
+    config : str
+        OSA configuration file to use.
+    test : bool
+
+    Returns
+    -------
+    list
+        Lines of the sequencer output.
+    """
+    log.info("Calling sequencer...")
     commandargs = [
         "sequencer",
         "-c",
-        args.osa_config_file,
+        config,
         "-s",
         "-d",
-        options.date,
+        date,
         options.tel_id,
     ]
+
+    if test:
+        commandargs.insert(-1, "-t")
 
     try:
         output = sp.run(
             commandargs, stdout=sp.PIPE, stderr=sp.STDOUT, encoding="utf-8", check=True
         )
     except sp.CalledProcessError as error:
-        print(
-            f"Command with the following args {commandargs} failed, {error.returncode}"
-        )
+        log.error(f"Command {commandargs} failed, {error.returncode}")
         sys.exit(1)
     else:
-        print(
-            f'<p>Processing data from: {date_to_iso(options.date)}. Last updated: '
-            f'{datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")} UTC</p>'
-        )
-
         # Strip newlines and fit it into a table:
-        lines = output.stdout.splitlines()
+        return output.stdout.splitlines()
 
-        if len(lines) > 1:
-            matrix = []
-            for line in lines:
-                l_fields = line.split()
-                if len(l_fields) == 18:
-                    # Full line, all OK
-                    matrix.append(l_fields)
 
-            matrix_to_html_table(matrix, column_class, True, False)
-        else:
-            # Show just plain text
-            print(f"<pre>{output.stdout}</pre>")
+def lines_to_matrix(lines: Iterable) -> list:
+    """Build the matrix from the sequencer output lines."""
+    matrix = []
+    for line in lines:
+        l_fields = line.split()
+        if len(l_fields) == 18:
+            matrix.append(l_fields)
+    return matrix
 
-    # Print the closing html
-    webtail()
+
+def matrix_to_html(matrix: list) -> str:
+    """Build the html table with the sequencer status report."""
+    log.info("Building the html table from sequencer output")
+    if len(matrix) < 2:
+        return "<p>No data found</p>"
+    df = pd.DataFrame(matrix[1:], columns=matrix[0])
+    return df.to_html(index=False)
+
+
+def main():
+    """Produce the html file with the processing status from the sequencer report."""
+
+    log.setLevel(logging.INFO)
+
+    args = sequencer_webmaker_argparser().parse_args()
+
+    if args.date:
+        flat_date = date_to_dir(args.date)
+        options.date = args.date
+
+    else:
+        # yesterday by default
+        yesterday = datetime.now() - timedelta(days=1)
+        options.date = yesterday
+        flat_date = date_to_dir(yesterday)
+
+    date = date_to_iso(options.date)
+
+    if is_day_closed():
+        log.info(f"Date {date} is already closed for {options.tel_id}")
+        sys.exit(1)
+
+    run_summary_directory = Path(cfg.get("LST1", "RUN_SUMMARY_DIR"))
+    run_summary_file = run_summary_directory / f"RunSummary_{flat_date}.ecsv"
+    if not run_summary_file.is_file():
+        log.error(f"No RunSummary file found for {date}")
+        sys.exit(1)
+
+    # Get the table with the sequencer status report:
+    lines = get_sequencer_output(date, args.config, args.test)
+
+    # Build the html sequencer table that will be place in the body of the HTML file
+    matrix = lines_to_matrix(lines)
+    html_table = matrix_to_html(matrix)
+
+    # Save the HTML file
+    log.info("Saving the HTML file")
+    directory = Path(cfg.get("LST1", "SEQUENCER_WEB_DIR"))
+    directory.mkdir(parents=True, exist_ok=True)
+
+    html_file = directory / Path(f"osa_status_{flat_date}.html")
+    html_file.write_text(html_content(html_table, date), encoding="utf-8")
+
+    log.info("Done")
 
 
 if __name__ == "__main__":
