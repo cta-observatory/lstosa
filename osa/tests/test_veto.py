c445ba132ca9d108058f193632592dd7d9c534e6
Daniel Morcuende <dmorcuen@ucm.es>
2022-06-10 17:12:59 +0200

    adapt the test to use last pedcal run

--- osa/tests/test_veto.py
+++ osa/tests/test_veto.py
@@ -3,15 +3,10 @@ from pathlib import Path
 
 def test_failed_history():
     from osa.veto import failed_history
-    good_history_file = Path(
-        './extra/history_files/sequence_LST1_04183.history'
-    )
-    bad_history_file = Path(
-        './extra/history_files/sequence_LST1_04183_failed.history'
-    )
-    one_line_history_file = Path(
-        './extra/history_files/sequence_LST1_04183_oneline.history'
-    )
+
+    good_history_file = Path('./extra/history_files/sequence_LST1_04183.history')
+    bad_history_file = Path('./extra/history_files/sequence_LST1_04183_failed.history')
+    one_line_history_file = Path('./extra/history_files/sequence_LST1_04183_oneline.history')
     one_line_history = failed_history(one_line_history_file)
     good_history_failed = failed_history(good_history_file)
     bad_history_failed = failed_history(bad_history_file)
@@ -23,13 +18,15 @@ def test_failed_history():
 
 def test_get_veto_list(sequence_list):
     from osa.veto import get_veto_list
+
     veto_list = get_veto_list(sequence_list)
     assert not veto_list
 
 
 def test_get_closed_list(sequence_list):
     from osa.veto import get_closed_list
+
     closed_list = get_closed_list(sequence_list)
-    seq_list = ['LST1_01806', 'LST1_01807', 'LST1_01808']
+    seq_list = ['LST1_01807', 'LST1_01808', 'LST1_01809']
     for sequence in seq_list:
         assert sequence in closed_list
