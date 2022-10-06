0ecef7be948a68ecd4c2df7a588ab385bff071cc
Maria Lainez <98marialainez@gmail.com>
2022-10-06 14:24:36 +0200

    remove unnecessary f-string

--- osa/scripts/calibration_pipeline.py
+++ osa/scripts/calibration_pipeline.py
@@ -165,7 +165,7 @@ def calibrate_charge(
     except:
         print(f"Failed. Return code {analysis_step.rc}")
         cmd.append("--filters=52")
-        print(f"Trying again by setting filters 52")
+        print("Trying again by setting filters 52")
         analysis_step = ChargeCalibrationStage(run=f"{pedcal_run_id:05d}", command_args=cmd)
         analysis_step.execute()
         return analysis_step.rc
