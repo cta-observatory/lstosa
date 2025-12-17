08e1ab67d948e52bca747c4af9519ba6853afc15
rcervinoucm <rcervino@ucm.com>
2025-12-17 17:45:19 +0100

    Update tests

--- src/osa/nightsummary/tests/test_database.py
+++ src/osa/nightsummary/tests/test_database.py
@@ -2,8 +2,7 @@ from osa.nightsummary import database
 
 def test_query():
     result = database.query(obs_id=20038)
-    assert result.source_name == "1ES_1101-232"
-    assert result.ra == 165.906
-    assert result.dec == -23.492
+    assert result is None
+
 
 
