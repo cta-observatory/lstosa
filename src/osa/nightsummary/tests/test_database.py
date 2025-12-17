ac7fffdc5b7425ecf28ffb7b91e893c5f41d6b30
rcervinoucm <rcervino@ucm.com>
2025-12-17 16:39:39 +0100

    Update database_test

--- src/osa/nightsummary/tests/test_database.py
+++ src/osa/nightsummary/tests/test_database.py
@@ -6,4 +6,4 @@ def test_query():
     from osa.nightsummary import database
 
     with pytest.raises(ConnectionFailure):
-        database.query(obs_id=1616, property_name="DriveControl_SourceName")
+        database.query(obs_id=20038)
