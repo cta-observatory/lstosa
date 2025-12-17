74f6378b1a0e9a655966b53beef2146d092fec0b
rcervinoucm <rcervino@ucm.com>
2025-12-17 17:16:35 +0100

    Update database tests

--- src/osa/nightsummary/tests/test_database.py
+++ src/osa/nightsummary/tests/test_database.py
@@ -4,6 +4,9 @@ from pymongo.errors import ConnectionFailure
 
 def test_query():
     from osa.nightsummary import database
+    result = database.query(obs_id=20038)
+    assert result.source_name == "1ES_1101-232"
+    assert result.ra == 165.906
+    assert result.dec == -23.492
 
-    with pytest.raises(ConnectionFailure):
-        database.query(obs_id=1616)
+
