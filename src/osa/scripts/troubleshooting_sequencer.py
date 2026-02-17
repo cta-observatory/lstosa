6765c69e201144ae536d3d859e7c3770b85d740d
rcervinoucm <rcervino@ucm.es>
2026-02-17 14:31:20 +0100

    Update troubleshooting_sequencer.py

--- src/osa/scripts/troubleshooting_sequencer.py
+++ src/osa/scripts/troubleshooting_sequencer.py
@@ -27,13 +27,22 @@ KNOWN_ERRORS = {
         "tag": "ValueError: x_new is above the interpolation range's maximum value",
         "msg": "Run must be discarded.",
         "id": 4
+    },
+    re.escape("No such file or directory: 'datasequence'"): {
+        
+        "tag": "No such file or directory: 'datasequence'",
+        "msg": "Command must be relaunched",
+        "id": 5
+    },
+    r"lstcam_calib_onsite_create_drs4_pedestal_file.*Output file exists already\. Stop": {
+        "action": "Remove the DRS4 file", 
+        "description": "the DRS4 file already exists",
+        "id":6
     }
-    # Poner error del Sabado, donde el log es /fefs/onsite/data/lst-pipe/LSTN-01/running_analysis/20260116/v0.11/log/Run23349.4294967294_jobid_51810134.err
-# ver como sacar el valor largo 4...294967294
 
 }
 
-def handle_error(job_id, job_name, state, log_path, error_path, command, logger_func, start_date, end_date):
+def handle_error(job_id, job_name, state, log_path, error_path, command, logger_func, start_date, end_date, handler):
     """
     Checks if a specific pattern (string or regex) exists in a log file.
     Returns: True if found, False otherwise.
@@ -65,33 +74,40 @@ def handle_error(job_id, job_name, state, log_path, error_path, command, logger_
 
 
     review_path = error_path
+    utils.run_command('osa-env')
 
     if state == "TIMEOUT":
         
         logger_func(f"   |__ ❌ DIAGNOSIS: TIMEOUT (Walltime exceeded).")
         logger_func(f"   |__ 💡 ACTION: Increase --mem in sbatch.")
         try:    
+            if command in handler:
+                utils.save_skipped_job_id(job_id)
+                return
+            
             success = utils.increase_memory_and_relaunch(command,30)
 
-            # --- GESTIÓN DEL ÉXITO ---
+            # --- SUCCESS MANAGEMENT ---
             if success:
-                logger_func(f"   |__ ✅ ÉXITO: Run {run_id} actualizada la memoria.")
+                logger_func(f"   |__ ✅ SUCCESS: Job relaunched")
                 
-                # Guardamos el Job ID para no volver a hacerlo
+                # Save the Job ID to avoid reprocessing
                 saved = utils.save_processed_job_id(job_id)
                 if saved:
-                    logger_func(f"   |__ 💾 GUARDADO: Job {job_id} registrado en historial.")
+                    logger_func(f"   |__ 💾 SAVED: Job {job_id} registered in history.")
                 else:
-                    logger_func(f"   |__ ⚠️ ERROR: No se pudo escribir en el historial de jobs.")
+                    logger_func(f"   |__ ⚠️ ERROR: Could not write to job history.")
             
             else:
-                # Si update_ecsv_cell devuelve False (fichero no encontrado, columna no existe, etc.)
-                logger_func(f"   |__ ⚠️ FALLO FUNCIONAL: No se pudo actualizar el comando")
+                # If update_ecsv_cell returns False (file not found, column doesn't exist, etc.)
+                logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Could not update the command")
+
+            return command
 
         except Exception as e:
-            # --- GESTIÓN DEL ERROR (Excepción de código) ---
-            logger_func(f"   |__ ❌ EXCEPCIÓN: Ocurrió un error inesperado gestionando Job {job_id}.")
-            logger_func(f"   |__ 🔍 Detalle: {str(e)}")
+            # --- ERROR MANAGEMENT (Code Exception) ---
+            logger_func(f"   |__ ❌ EXCEPTION: An unexpected error occurred managing Job {job_id}.")
+            logger_func(f"   |__ 🔍 Detail: {str(e)}")
         return
     
     if not log_path or not os.path.exists(log_path):
@@ -130,30 +146,30 @@ def handle_error(job_id, job_name, state, log_path, error_path, command, logger_
 
                             if subrun_id == utils.get_ecsv_column_value(ecsv_path, run_id, 'n_subruns'):
 
-                                # Intentamos actualizar el ECSV
+                                # Attempt to update the ECSV
                                 success = utils.update_ecsv_cell(ecsv_path, run_id, "n_subruns", subrun_id-1)
 
-                                # --- GESTIÓN DEL ÉXITO ---
+                                # --- SUCCESS MANAGEMENT ---
                                 if success:
-                                    logger_func(f"   |__ ✅ ÉXITO: Run {run_id} actualizado a EDATA en ECSV.")
+                                    logger_func(f"   |__ ✅ SUCCESS: Run {run_id} updated to EDATA in ECSV.")
                                     
-                                    # Guardamos el Job ID para no volver a hacerlo
+                                    # Save the Job ID to avoid reprocessing
                                     saved = utils.save_processed_job_id(job_id)
                                     if saved:
-                                        logger_func(f"   |__ 💾 GUARDADO: Job {job_id} registrado en historial.")
+                                        logger_func(f"   |__ 💾 SAVED: Job {job_id} registered in history.")
                                     else:
-                                        logger_func(f"   |__ ⚠️ ERROR: No se pudo escribir en el historial de jobs.")
+                                        logger_func(f"   |__ ⚠️ ERROR: Could not write to job history.")
                                 
                                 else:
-                                    # Si update_ecsv_cell devuelve False (fichero no encontrado, columna no existe, etc.)
-                                    logger_func(f"   |__ ⚠️ FALLO FUNCIONAL: No se pudo actualizar el ECSV (verifica rutas o columnas).")
+                                    # If update_ecsv_cell returns False (file not found, column doesn't exist, etc.)
+                                    logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Could not update the ECSV (check paths or columns).")
 
                             else:
-                                logger_func(f"   |__ ⚠️ FALLO FUNCIONAL: Subrun {subrun_id} no es el último subrun de la Run {run_id}. No se actualiza el Run Summary.")
+                                logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Subrun {subrun_id} is not the last subrun of Run {run_id}. Run Summary not updated.")
                         except Exception as e:
-                            # --- GESTIÓN DEL ERROR (Excepción de código) ---
-                            logger_func(f"   |__ ❌ EXCEPCIÓN: Ocurrió un error inesperado gestionando Job {job_id}.")
-                            logger_func(f"   |__ 🔍 Detalle: {str(e)}")
+                            # --- ERROR MANAGEMENT (Code Exception) ---
+                            logger_func(f"   |__ ❌ EXCEPTION: An unexpected error occurred managing Job {job_id}.")
+                            logger_func(f"   |__ 🔍 Detail: {str(e)}")
 
                         return
                     if id == 4:
@@ -185,11 +201,71 @@ def handle_error(job_id, job_name, state, log_path, error_path, command, logger_
                             logger_func(f"   |__ ❌ EXCEPTION: An unexpected error occurred managing Job {job_id}.")
                             logger_func(f"   |__ 🔍 Detail: {str(e)}")
                         return
+                    if id == 5:
+                        try:
+                            if command in handler:
+                                utils.save_skipped_job_id(job_id)
+                                return
+                            success = utils.increase_memory_and_relaunch(command, 30)
+
+                            # --- SUCCESS MANAGEMENT ---
+                            if success:
+                                logger_func(f"   |__ ✅ SUCCESS: Job relaunched")
+                                
+                                
+                                # Save the Job ID to avoid reprocessing
+                                saved = utils.save_processed_job_id(job_id)
+                                if saved:
+                                    logger_func(f"   |__ 💾 SAVED: Job {job_id} registered in history.")
+                                else:
+                                    logger_func(f"   |__ ⚠️ ERROR: Could not write to job history.")
+
+                            
+                            else:
+                                # If function returns False
+                                logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Could not update the command.")
+                            return command
+
+                        except Exception as e:
+                            # --- ERROR MANAGEMENT (Code Exception) ---
+                            logger_func(f"   |__ ❌ EXCEPTION: An unexpected error occurred managing Job {job_id}.")
+                            logger_func(f"   |__ 🔍 Detail: {str(e)}")
+
+                    if id == 6:
+                        try:
+                            if command in handler:
+                                utils.save_skipped_job_id(job_id)
+                                return
+                            # Ensure 'yesterday' is defined, as it might only be defined in previous blocks
+                            yesterday = datetime.now() - timedelta(days=1)
+                            summary_date = yesterday.strftime('%Y%m%d')
+                            success = utils.delete_path(f"/fefs/onsite/data/lst-pipe/LSTN-01/monitoring/PixelCalibration/Cat-A/drs4_baseline/{summary_date}/v0.1.1/drs4_pedestal*.h5")
+
+                            # --- SUCCESS MANAGEMENT ---
+                            if success:
+                                logger_func(f"   |__ ✅ SUCCESS: Job relaunched")
+                                
+                                success = utils.increase_memory_and_relaunch(command, 30)
+                                if success:
+                                    # Save the Job ID to avoid reprocessing
+                                    saved = utils.save_processed_job_id(job_id)
+                                    if saved:
+                                        logger_func(f"   |__ 💾 SAVED: Job {job_id} registered in history.")
+                                    else:
+                                        logger_func(f"   |__ ⚠️ ERROR: Could not write to job history.")
+                                else:
+                                    # If function returns False
+                                    logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Could not relaunch the command.")
+                            else:
+                                # If function returns False
+                                logger_func(f"   |__ ⚠️ FUNCTIONAL FAILURE: Could not remove the DRS4 file.")
+                            return command
+
+                        except Exception as e:
+                            # --- ERROR MANAGEMENT (Code Exception) ---
+                            logger_func(f"   |__ ❌ EXCEPTION: An unexpected error occurred managing Job {job_id}.")
+                            logger_func(f"   |__ 🔍 Detail: {str(e)}")
                     
     except Exception as e:
         print(f"[UTILS ERROR] Failed to read log {error_path}: {e}")
     return None, None
-
-
-
-
