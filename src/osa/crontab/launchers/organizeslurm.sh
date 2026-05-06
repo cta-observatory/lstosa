487419af179aecc5ec510e36e6e9abd62e42a260
Manuel19032002 <manuelmartinezherresanchez@gmail.com>
2026-05-06 13:42:34 +0200

    Update organizeslurm.sh

--- src/osa/crontab/launchers/organizeslurm.sh
+++ src/osa/crontab/launchers/organizeslurm.sh
@@ -9,5 +9,5 @@
 #OBS_DATE=2020-01-17 example
 source /local/home/lstanalyzer/osa-env.sh
 
-/usr/bin/bash /fefs/aswg/workspace/manuel.martinezherresanchez/lstosa/src/osa/crontab/launchers/launch_organize.sh 
+/usr/bin/bash /fefs/aswg/lstosa/src/osa/crontab/launchers/launch_organize.sh 
 # options: -s --no-gainsel --no-running
